b13538ba498fea54fd9f147b4ec51378528034af
anna-rose <917566@isd624.org>
2018-07-25 20:59:55 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -4,7 +4,27 @@ nickels = 0
 pennies = 0 
 
 def least_coins(cents)
-coins = {
-#Code your answer here!
-
+coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+quarters = cents / 25
+  if quarters == 0 
+    coins[:quarters] = 0 
+  elsif quarters != 0 
+    coins[:quarters] = quarters
+    cents = cents - quarters * 25
+  end 
+dimes = cents / 10
+  if dimes == 0 
+    coins[:dimes] = 0 
+  elsif dimes != 0 
+    coins[:dimes] = dimes
+    cents = cents - dimes * 10
+  end
+  nickles = cents / 5
+  if nickels == 0 
+    coins[:nickels] = 0 
+  elsif nickels != 0 
+    coins[:nickels] = nickels
+    cents = cents - mickels * 5
+  end 
+  
 end

86d689fba87eeb825ebe8f708c9b331196ec92d3
anna-rose <917566@isd624.org>
2018-07-25 21:04:47 +0000

    automatically backed up by learn

--- least_coins.rb
+++ least_coins.rb
@@ -1,10 +1,6 @@
-quarters = 0 
-dimes = 0 
-nickels = 0 
-pennies = 0 
-
 def least_coins(cents)
 coins = {:quarters => 0, :dimes => 0, :nickels => 0, :pennies => 0}
+
 quarters = cents / 25
   if quarters == 0 
     coins[:quarters] = 0 
